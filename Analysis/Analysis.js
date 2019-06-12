cbf88a9f82be4b53366a5e2f7e0bb5492ed0393c
MarinaBoudin <marina.boudin@etu.u-bordeaux.fr>
2019-06-12 15:28:59 +0200

    Post in Get

--- Analysis/Analysis.js
+++ Analysis/Analysis.js
@@ -10,7 +10,7 @@ function result_python(response){
 function call_python(file_name){
   $.ajax({
     url:'~/Test.py',
-    type:'POST',
+    type:'GET',
     data:{ param:file_name},
     success:function(response){
       result_python(response);
